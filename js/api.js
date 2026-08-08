2acc8386d36c469162305d4c8a6575bc4ca088c5
yonsaplikasi <yonsaplikasi@gmail.com>
2026-08-08 18:45:30 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 // Gantilah URL di bawah ini dengan URL Web App Apps Script Anda
-const API_URL = "https://script.google.com/macros/s/AKfycbx4tNKDcIyfsYxRG_0b4zCHNpIIsVCI2AiPa1jP2O4UN5UJkqvReyPT5q324j_PUpjTFg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbx_yJt60ZV2pd0SZxTaVyujOiQN-G7s_eyc0QfSKZMwOPzpxgDyEl2OgCHQU_hXzfYK5Q/exec";
 
 const RestoAPI = {
   async getMenu() {
